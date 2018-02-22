07852c67e10bda3f85a9002dec9844b5a63f8e51
dogancankilment <dogancankilment@gmail.com>
2018-02-23 01:56:31 +0300

    register function using UserCreateForm at forms.py file. This form is a modelform, instance user class from models.py, and using mail sender utils function

---
+++ authentication/forms.py
@@ -0,0 +1,33 @@
+from django.forms import ModelForm, forms
+from django import forms
+from .models import User
+from django.contrib.auth.forms import UserCreationForm
+
+from utils.mail_sender import mail_sender
+
+
+class UserCreateForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+
+        if email and User.objects.filter(email=email).exclude(username=username).count():
+            raise forms.ValidationError(u'Email addresses must be unique.')
+        return email
+
+    def save(self, commit=True):
+        user = super(UserCreateForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+
+        user.is_active = False
+
+        if commit:
+            user.save()
+            mail_sender(user.email)
+        return user
