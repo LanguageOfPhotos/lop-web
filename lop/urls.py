eb32f56a470ef16268f7d634da13e5eb5c8fead1
dogancankilment <dogancankilment@gmail.com>
2018-02-23 03:39:27 +0300

    new urls for authentication app

--- lop/urls.py
+++ lop/urls.py
@@ -1,17 +1,31 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'lop.views.home', name='home'),
-    # url(r'^lop/', include('lop.foo.urls')),
+                       # home urls
+                       url(r'^$',
+                           'lop.views.home',
+                           name='home'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # authentication app urls
+                       url(r'^login',
+                           'authentication.views.login',
+                           name='login'),
+                       url(r'^register',
+                           'authentication.views.register',
+                           name='register'),
+                       )
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += patterns('',
+                            (r'^media/(?P<path>.*)$',
+                             'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}))
+
+if settings.DEBUG:   # if DEBUG is True it will be served automatically
+    urlpatterns += patterns('',
+                            url(r'^static/(?P<path>.*)$',
+                                'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+                            )
