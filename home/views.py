dc88c6e00da344f0b8577b79e2c42a855415d0d7
dogancankilment <dogancankilment@gmail.com>
2018-02-26 09:38:56 +0300

    home page function created

--- home/views.py
+++ home/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.core.context_processors import csrf
+
+
+def home(request):
+    c = {'request': request}
+    c.update(csrf(request))
+
+    return render_to_response("home/index.html", c)
