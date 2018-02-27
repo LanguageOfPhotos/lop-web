f767e9695fa64fd571560b4eb1ffa683a6f31ee5
dogancankilment <dogancankilment@gmail.com>
2018-02-27 10:09:10 +0300

    Image model class created for every image object

--- adminInterface/models.py
+++ adminInterface/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Image(models.Model):
+    title = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    which_user = models.ForeignKey(User)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    image = models.ImageField(upload_to='images/',
+                              default='images/default.jpg')
+
+    is_verified = models.BooleanField(default=False)
+
+
+    class Meta:
+        ordering = ['-updated_at']
