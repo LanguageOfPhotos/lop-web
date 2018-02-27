40672dd5745e4b9035e753b5519668a35b3d0af5
dogancankilment <dogancankilment@gmail.com>
2018-02-27 10:09:53 +0300

    adminInterface new image function created for post and save process from form data

--- adminInterface/views.py
+++ adminInterface/views.py
@@ -1,1 +1,30 @@
-# Create your views here.
+from django.shortcuts import render, render_to_response, redirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.core.context_processors import csrf
+from .models import Image
+from authentication.models import User
+from home.views import home
+
+
+@login_required()
+def newimage(request):
+    if request.POST:
+        post = Image()
+
+        post.title = request.POST['title']
+        post.country = request.POST['country']
+        post.category = request.POST['category']
+        post.which_user = request.user
+
+        if request.FILES:
+            post.image = request.FILES['image']
+
+        post.save()
+
+        return redirect(reverse(home))
+
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render_to_response("admininterface/new-image.html",c)
