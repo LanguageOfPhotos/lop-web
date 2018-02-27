03b5849b9d9b9a4b67519ae5561ec4c16e5f37b9
dogancankilment <dogancankilment@gmail.com>
2018-02-27 10:10:32 +0300

    ORM line added for run all images sql query

--- home/views.py
+++ home/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
+from adminInterface.models import Image
 
 
 def home(request):
-    c = {'request': request}
+    all_images = Image.objects.all()
+
+    c = {'request': request,
+         'all_images': all_images}
+
     c.update(csrf(request))
 
     return render_to_response("home/index.html", c)
